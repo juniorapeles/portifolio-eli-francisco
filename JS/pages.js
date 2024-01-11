cc6d9670b6552b91a5798ca55ff550b24968ded0
eli <eli.apeles@itpower.co>
2024-01-11 01:17:25 -0300

    feat - botao muda cor

--- JS/pages.js
+++ JS/pages.js
@@ -1,3 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const elementosAnimar = document.querySelectorAll('.animacao');
+    elementosAnimar.forEach(function (elemento) {
+        elemento.classList.add('aparecer');
+    });
+});
+
+function toggleContent() {
+    const titulo = document.querySelector('.homeSection h2');
+    const habilidadesContainer = document.querySelector('.skills-container');
+
+    // Verifica se o modo escuro está ativado
+    const isDarkMode = document.body.classList.contains('dark-mode');
+
+    // Define os textos conforme o modo atual
+    if (isDarkMode) {
+        titulo.textContent = 'Desenvolvedor Full Stack';
+        habilidadesContainer.innerHTML = '<span>HTML/CSS/Bootstrap</span><span>Java</span><span>Spring</span><span>Jpa</span><span>JUnit</span><span>Mockito</span>';
+    } else {
+        titulo.textContent = 'Desenvolvedor Backend';
+        habilidadesContainer.innerHTML = '<span>Coldfusion</span><span>SqlServer</span><span>HTML/CSS/Bootstrap</span><span>Java</span><span>Spring</span><span>Jpa</span><span>JUnit</span><span>Mockito</span>';
+    }
+}
+
 function mudaFrame(secao) {
     const homeSection = document.querySelectorAll('.homeSection');
     const projetosSection = document.getElementById('projetosSection');
@@ -31,3 +55,20 @@ function mudaFrame(secao) {
         console.error("Algum dos elementos não foi encontrado!");
     }
 }
+
+const body = document.body;
+const footer = document.querySelector('footer');
+
+function toggleDarkMode() {
+    body.classList.toggle('dark-mode');
+    footer.classList.toggle('dark-mode');
+}
+
+const darkModeButton = document.createElement('button');
+darkModeButton.innerHTML = 'Modo Escuro';
+darkModeButton.classList.add('btn', 'btn-dark', 'fixed-bottom', 'mb-3', 'me-3');
+darkModeButton.addEventListener('click', function () {
+    toggleDarkMode();
+    toggleContent(); // Adiciona esta linha para ajustar o conteúdo quando muda para o modo escuro
+});
+
