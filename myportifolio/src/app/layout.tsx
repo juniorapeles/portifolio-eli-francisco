bb75fcabb5d021b5f31788a71bb57a34ef2f0e16
eli <elijunior.apeles2005@gmail.com>
2024-03-27 23:49:16 -0300

    feat - initial commit

---
+++ myportifolio/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.scss";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Eli Francisco",
+  description: "Eli Francisco is a software enginner...",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
