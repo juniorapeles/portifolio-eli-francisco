0984ad3fdbb4973c2a6e84d48bbc98e832399d02
eli <elijunior.apeles2005@gmail.com>
2024-04-08 20:33:30 -0300

    era um ponto

--- src/app/components/experience/Experience.tsx
+++ src/app/components/experience/Experience.tsx
@@ -10,10 +10,10 @@ export function Experience(){
           <SectionTitle text="Experience"/>
           <p>1 year working as a Software Developer, in Itpower Software LTDA.</p>
           <div className="experience-time">
-            <Skill image="/react.png" measure={1} years="1 year"/>
-            <Skill image="/ts.png" measure={1} years="1 year"/>
-            <Skill image="/js.png" measure={1} years="1 year"/>
-            <Skill image="/java.png" measure={1} years="1 year"/>
+            <Skill image="./react.png" measure={1} years="1 year"/>
+            <Skill image="./ts.png" measure={1} years="1 year"/>
+            <Skill image="./js.png" measure={1} years="1 year"/>
+            <Skill image="./java.png" measure={1} years="1 year"/>
           </div>
         </div>
     )
