7d720b692d6845b2f25079bfbc6644c70681d931
eli <elijunior.apeles2005@gmail.com>
2025-06-08 14:58:48 -0300

    feat npm solta a carta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,22 +18,12 @@ export default function Home() {
         </section>
         
       <section id="projetos" className={styles.container}>
-        {/* <Projects 
-          title="Finance - API Financeira" 
-          description="API desenvolvida em Java 17 com Spring Boot para controle financeiro pessoal. Projeto dockerizado, com autenticação JWT, integração com bancos de dados relacionais, e arquitetura voltada para escalabilidade." 
-        /> */}
-        
         <Projects 
           title="House of Books" 
           description="Sistema completo para gerenciamento de livros e editoras, construído com Java 11, Spring Boot e PostgreSQL. Inclui autenticação, CRUD completo, boas práticas RESTful e testes com Insomnia." 
           codeLink="https://github.com/juniorapeles/House-Of-Books"
           projectLink="https://house-of-books.juniorapeles.xyz/"
         />
-        
-        {/* <Projects 
-          title="Sistema de CRM Integrado" 
-          description="Solução backend para integração entre CRM e ERP, com foco em performance e escalabilidade. Utiliza Redis para caching e Docker para gerenciamento de ambientes." 
-        /> */}
       </section>
         
         <section id="sobre">
